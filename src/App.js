6877482238b3fdb44829efce9348285ed62a73b0
Uchey <ucheuzochukwumark@gmail.com>
2020-03-25 21:02:07 +0100

    intro redux module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import store from './store/index';
+// import PrivateRoute from './';
+import Dashboard from './containers/dashboard/dashboard'
+import Signin from './containers/login/login';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     <h1>TeamWork</h1>
-    </div>
+   <Provider store={store}>
+     <Router>
+       <Switch>
+          {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
+       </Switch>
+       <Route exact path="/" component={Signin} />
+     </Router>
+   </Provider>
   );
 }
 
