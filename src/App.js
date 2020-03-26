2210612fb10ea523a6979cafe958bcb87ba46787
Uchey <ucheuzochukwumark@gmail.com>
2020-03-26 12:52:41 +0100

    nav-bar atom set-up

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import store from './store/index';
+// import { Provider } from 'react-redux';
+import {  BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+// import store from './store/index';
 // import PrivateRoute from './';
-import Dashboard from './containers/dashboard/dashboard'
+// import Dashboard from './containers/dashboard/dashboard';
 import Signin from './containers/login/login';
+//components
+import NavBar from './components/navBar/navBar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-   <Provider store={store}>
-     <Router>
+    <Router>
+       <NavBar />
+      <div className="container">
        <Switch>
           {/* <PrivateRoute path="/dashboard" component={Dashboard} /> */}
+          {/* <Route exact path="/" component={Signin} /> */}
        </Switch>
-       <Route exact path="/" component={Signin} />
-     </Router>
-   </Provider>
+      </div>
+    </Router>
+
   );
 }
 
